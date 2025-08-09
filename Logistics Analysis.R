b2a445d5a75f0f6f6d7d1c82093ff47e256e54f3
Maiar0 <dennis.ward2nd@gmail.com>
2025-08-08 20:06:05 -0500

    working

--- Logistics Analysis.R
+++ Logistics Analysis.R
@@ -6,14 +6,15 @@ logistics <- read_csv("Logistics Data/incom2024_delay_example_dataset.csv")#unpa
 logistics <- logistics %>%
   mutate(year = year(ymd_hms(order_date)))#extract year of line item
 
-#Shipment Cost Per Region
-shipment_cost_region <- logistics %>%
+#profit ratio per region
+profit_per_region <- logistics %>%
   group_by(order_region) %>%
-  summarise(avg_shipment_cost = mean(shipment_cost, na.rm = TRUE))
+  summarise(avg_profit_ratio = mean(order_item_profit_ratio, na.rm = TRUE))
 
-ggplot(shipment_cost_region, aes(x=reorder(region,-avg_shipment_cost),y=avg_shipment_cost)) +
+ggplot(profit_per_region, aes(x=reorder(order_region,-avg_profit_ratio),y=avg_profit_ratio)) +
   geom_col(fill="steelblue") +
-  labs(title= "Average Shipment Cost per Region", x = "Region", y = "Average Cost") +
-  theme_minimal()
+  labs(title= "Average Profit per Region Ratio", x = "Region", y = "Average Profit Ratio") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 
